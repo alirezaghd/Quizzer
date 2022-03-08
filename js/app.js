414b6decfddef4fee7aa35a5a995ffe9b5eaefda
alirezagholamrezaei <alireza.gh.d@gmail.com>
2022-03-08 17:01:45 +0330

    second1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 
 function add_answers(){
 
-
+  var answer_Att;
   var new_input = document.createElement("INPUT");
   new_input.setAttribute("type", "text");
+  for (let i = 0; i < 6; i++){
+      answer_Att = "answer" + [i]
 
-  new_input.setAttribute("name", "answer");
+  }
+  new_input.setAttribute("name", answer_Att);
 
   new_input.className = "form-control mt-2"
 
